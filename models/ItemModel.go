bf47f39f11c603add9db355f7fab97c9358c7ae5
Emir Kovacevic <32126521+EmirWorld@users.noreply.github.com>
2023-07-03 18:23:58 +0200

    Remove quantity from item

--- models/ItemModel.go
+++ models/ItemModel.go
@@ -16,7 +16,6 @@ type ItemNew struct {
 	Name        string `json:"name" bson:"name" validate:"required" example:"My Item"`
 	Description string `json:"description" bson:"description" validate:"required" example:"My Item Description"`
 	Price       Price  `json:"price" bson:"price" validate:"required"`
-	Quantity    int    `json:"quantity" bson:"quantity" validate:"required" example:"123" default:"1"`
 }
 
 type Item struct {
@@ -24,7 +23,6 @@ type Item struct {
 	Name           string             `json:"name" bson:"name" validate:"required" example:"My Item"`
 	Description    string             `json:"description" bson:"description" validate:"required" example:"My Item Description"`
 	Price          Price              `json:"price" bson:"price" validate:"required"`
-	Quantity       int                `json:"quantity" bson:"quantity" validate:"required" example:"123" default:"1"`
 	OrganisationID primitive.ObjectID `json:"organisation_id" bson:"organisation_id" validate:"required"`
 	UpdatedAt      time.Time          `json:"updated_at" bson:"updated_at"`
 	CreatedAt      time.Time          `json:"created_at" bson:"created_at"`
